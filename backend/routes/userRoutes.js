5e0d0f7ccc85bbe3b4f8640cd24861ed564df225
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-06 19:02:42 +0530

    Adding authMiddleware.js file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
+
 
 // @route POST /api/users/register
 // @desc Register a new user
@@ -39,13 +40,68 @@ router.post("/register", async (req, res) => {
       token,
       })
       
-    });
+     }
+);
     
 
   } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
   }
+});
+
+// @route POST /api/users/login
+// @desc Authenticate user
+// @access Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try{
+    // Find user by email
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch)
+         return res.status(400).json({ message: "Invalid Credentials" });
+
+      // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    //Sign and return JWT
+    jwt.sign(
+        payload,
+         process.env.JWT_SECRET,
+         {expiresIn: "40h"}, 
+         (err, token) => {
+      if (err) throw err;
+
+      res.json({
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+      },
+      token,
+      })
+      
+     }
+);
+
+} catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+
+}
+});
+
+// @route GET /api/users/profile
+// @desc Get logged-in user's profile (Protected Route)
+// @access Private
+router.get("/profile",protect, async (req, res) => {
+    res.json(req.user);
 });
 
 module.exports = router;
