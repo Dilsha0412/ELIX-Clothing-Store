f8977a05c1a934ff8209067b2f7d93a8fe835bc3
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-11 22:51:46 +0530

    Build seeder.js file

---
+++ backend/seeder.js
@@ -0,0 +1,46 @@
+const Product = require("./models/Product");
+const User = require("./models/User");
+const products = require("./data/products");
+const mongoose = require('mongoose'); // mongoose require කරන්න
+const dotenv = require('dotenv');     // dotenv require කරන්න
+
+dotenv.config();
+
+// Connect to mongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+// Function to seed data
+
+const seedData = async () => {
+  try {
+    // Clear existing data
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // Create a default admin User
+    const createdUser = await User.create({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "123456",
+      role: "admin",
+    });
+
+    // Assign the default user ID to each product
+    const userID = createdUser._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userID };
+    });
+
+    // Insert the products into the database
+    await Product.insertMany(sampleProducts);
+
+    console.log("Product data seeded successfully!");
+    process.exit();
+  } catch (error) {
+    console.error("Error seeding the data:", error);
+    process.exit(1);
+  }
+};
+
+seedData();
