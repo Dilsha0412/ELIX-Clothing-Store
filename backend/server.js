a4a3db04355ebcf5c81910406df3a63a40ae82aa
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-19 13:42:40 +0530

    feat: add subscriber route and integrate with server

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cartRoutes = require('./routes/cartRoutes');
 const checkoutRoutes = require('./routes/checkoutRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
-
+const subscribeRoute = require("./routes/subscribeRoute");
 
 const app = express();
 app.use(express.json());
@@ -33,6 +33,7 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/checkout', checkoutRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
+app.use('/api', subscribeRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
