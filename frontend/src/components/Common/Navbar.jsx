4eb281b26d353770c413573485b4c29556030fa7
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-22 02:16:46 +0530

    style: update Navbar styling and responsiveness fixes

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -10,6 +10,9 @@ import { IoMdClose } from 'react-icons/io';
 const Navbar = () => {
     const[drawerOpen, setDrawerOpen] = useState(false);
     const[navDrawerOpen, setNavDrawerOpen] = useState(false);
+    const { cart } = useSelector((state) => state.cart);
+
+    const cartItemCount = cart?.products?.reduce((total, product) => total + product.quantity, 0) || 0;
 
     const toggleNavDrawer = () => {
         setNavDrawerOpen(!navDrawerOpen);
@@ -34,25 +37,24 @@ const Navbar = () => {
 
         <div className='hidden md:flex space-x-6'>
             <Link
-             to="/collections/all"
+             to="/collections/all?gender=Men"
              className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
                 Men
             </Link>
 
             <Link 
-            to="#"
+            to="/collections/all?gender=Women"
             className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
                 Women
             </Link>
-
             <Link
-             to="#" 
+             to="/collections/all?category=Top Wear"
              className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
                 Top Wear
             </Link>
 
             <Link 
-            to="#" 
+            to="/collections/all?category=Bottom Wear"
             className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
                 Bottom Wear
             </Link>
@@ -73,10 +75,15 @@ const Navbar = () => {
 
             <button 
             onClick={toggleCartDrawer} 
-            className='relative hover:text-black'>
+            className='relative hover:text-black'
+            >
+
             <HiOutlineShoppingBag className='h-6 w-6 text-gray-700'/>
-            <span className='absolute -top-1 bg-[#ea2e0e] text-white text-xs rounded-full px-2 py-0.5'>4
+                {cartItemCount > 0 && (<span className='absolute -top-1 bg-[#ea2e0e] text-white text-xs rounded-full px-2 py-0.5'>
+                {cartItemCount}
             </span>
+            )}
+            
             </button>
 
             <div className='overflow-hidden'>
@@ -104,19 +111,19 @@ const Navbar = () => {
             <div className='p-4'>
                 <h2 className='text-xl font-semibold mb-4'>Menu</h2>
                 <nav className="space-y-4">
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?gender=Men" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                     Men
                     </Link>
 
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?gender=Women" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                     Women
                     </Link>
 
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?category=Top Wear" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                     Top Wear
                     </Link>
 
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?category=Bottom Wear" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                     Bottom Wear
                     </Link>
                 </nav>
