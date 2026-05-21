145a1639c1868200906b823cbef94ef2c952c96b
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-22 02:16:08 +0530

    feat(cart): add dynamic quantity adjusters and auth conditional checkout navigation

--- frontend/src/components/Cart/CartContents.jsx
+++ frontend/src/components/Cart/CartContents.jsx
@@ -1,40 +1,49 @@
 import React from 'react'
 import { RiDeleteBin3Line } from 'react-icons/ri'
 
-const CartContents = () => {
+const CartContents = ({ cart ,userId ,guestId }) => {
+  const dispatch = useDispatch();
 
-  const cartProducts=[
-    {
-      productId:1,
-      name:"T-shirt",
-      size:"M",
-      color:"Red",
-      quentity:1,
-      price:15,
-      image:"https://picsum.photos/200?random=1",
-    },
+ // Handle adding or substracting to cart
+const handleAddToCart = (productId, delta, quantity, size, color) => {
+    const newQuantity = quantity + delta;
+    if (newQuantity >= 1) {
+        dispatch(
+            updateCartItemQuantity({
+                productId,
+                quantity: newQuantity,
+                guestId,
+                userId,
+                size,
+                color,
+            })
+        );
+    }
+};
 
-    {
-      productId:2,
-      name:"Jeans",
-      size:"L",
-      color:"Purple",
-      quentity:1,
-      price:15,
-      image:"https://picsum.photos/200?random=2",
-    },
-  ]
+const handleRemoveFromCart = (productId, size, color) => {
+    dispatch(
+        removeFromCart({
+            productId,
+            guestId,
+            userId,
+            size,
+            color,
+        })
+    );
+};
+
   return (
     <div>
-      {cartProducts.map((product,index)=>(
+      {cart?.products?.map((product,index)=>(
         <div
         key={index}
         className='flex items-start justify-between py-4 border-b '
         >
           <div className='flex items-start'>
             <img src={product.image} 
-            alt={product.name} 
-            className='w-20 h-24 object-cover'
+                 alt={product.name} 
+                 className='w-20 h-24 object-cover'
             />
             <div>
               <h3>{product.name}</h3>
@@ -42,12 +51,14 @@ const CartContents = () => {
                 size: {product.size} | color: {product.color} | 
               </p>
               <div className='flex items-center mt-2'>
-                <button className='border rounded px-2 py-1 text-xl font-medium'>
+                <button onClick={() => handleAddToCart(product.productId, -1, product.quantity, product.size, product.color)}
+                className='border rounded px-2 py-1 text-xl font-medium'>
                   -
                 </button>
-                <span className='mx-4'>{product.quentity}</span>
+                <span className='mx-4'>{product.quantity}</span>
 
-                <button className='border rounded px-2 py-1 text-xl font-medium'>
+                <button onClick={() => handleAddToCart(product.productId, 1, product.quantity, product.size, product.color)}
+                className='border rounded px-2 py-1 text-xl font-medium'>
                   +
                 </button>
 
@@ -56,7 +67,8 @@ const CartContents = () => {
           </div>
           <div>
             <p>$ {product.price.toLocaleString()}</p>
-            <button>
+            <button onClick={() => handleRemoveFromCart(product.productId, product.size, product.color)}
+            >
               <RiDeleteBin3Line className='h-6 w-6 mt-2 text-red-600'/>
             </button>
           </div>
