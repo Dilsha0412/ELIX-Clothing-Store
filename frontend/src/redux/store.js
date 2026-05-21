75be17079e460efffb4bda5ee0064c64b32e4501
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-21 22:33:39 +0530

    feat: implement checkout slice with async thunk for session creation

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "./slices/authSlice";
 import productsReducer from "./slices/productsSlice"; 
 import cartReducer from "./slices/cartSlice";
+import checkoutReducer from "./slices/checkoutSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productsReducer,
     cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 
