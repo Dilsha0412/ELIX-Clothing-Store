46b64b3c91ebbe580ccebbcd5d5ad3a084c89359
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-06-05 14:07:01 +0530

    feat: implement new arrivals product section with scrolling carousel and quick view capabilities

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Header from '../Common/Header';
 import Footer from '../Common/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const UserLayout = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <>
       <Header />
